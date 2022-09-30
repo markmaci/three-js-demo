c477a3c6a231f5be5962d01ed79e2e6bc1e15df2
Mark Maci <markmaci@crc-dot1x-nat-10-239-87-107.bu.edu>
2022-09-29 22:07:52 -0400

    clean up

--- src/script.js
+++ src/script.js
@@ -6,31 +6,21 @@ import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 
-/**
- * Base
- */
+
 // Debug
 const gui = new dat.GUI()
-
-
-// Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Scene
 const scene = new THREE.Scene()
 
 const axesHelper = new THREE.AxesHelper()
 // scene.add(axesHelper)
 
-/**
- * Textures
- */
 const textureLoader = new THREE.TextureLoader()
 const matcapTexture = textureLoader.load('/textures/matcaps/9.png')
 console.log(matcapTexture);
 
 
-// Fonts
 const fontLoader = new FontLoader()
 
 fontLoader.load(
@@ -95,52 +85,37 @@ fontLoader.load(
             console.timeEnd('donuts')
         
         
-
-        
     }
 )
 
-
-/**
- * Sizes
- */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
 window.addEventListener('resize', () =>
-{
-    // Update sizes
+{ 
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
-    // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-    // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
+
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 2
 scene.add(camera)
 
-// Controls
+
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Renderer
- */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
@@ -155,14 +130,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update controls
     controls.update()
-
-    // Render
     renderer.render(scene, camera)
 
-    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
